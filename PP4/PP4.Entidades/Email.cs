9a11338c13784981c6f002a506d6dc9439456cf6
E <E@E6440>
2019-12-16 11:54:30 -0600

    Restaleciemiento Contraseña, se agrego  clase Email, en entidades

---
+++ PP4/PP4.Entidades/Email.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E
+{
+    [Serializable]
+    public class CorreoElectronico
+    {
+        //Atributos
+        private string correoDestinatario;
+        private string correoRemitente;
+        private string asunto;
+        private string mensaje;
+
+        public string DESTINATARIO
+        {
+            get { return this.correoDestinatario; }
+            set { this.correoDestinatario = value; }
+        }
+
+        public string REMITENTE
+        {
+            get { return this.correoRemitente; }
+            set { this.correoRemitente = value; }
+        }
+
+        public string ASUNTO
+        {
+            get { return this.asunto; }
+            set { this.asunto = value; }
+        }
+
+        public string MENSAJE
+        {
+            get { return this.mensaje; }
+            set { this.mensaje = value; }
+        }
+
+    }
+}
---
+++ PP4/WebApplication/Restore_Password.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Restore_Password.aspx.cs" Inherits="PP4.Restore_Password" %>
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta charset="utf-8" />
+    <title></title>    
+</head>
+<body>
+    <form id="form1" runat="server">   
+    </form>
+</body>
+</html>
